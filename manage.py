08568bd3e9527f82324ef0c2b81c72ca9e507b2a
A. Kotelnikov <kotelnikov@enhancelab.ru>
2018-10-31 21:06:00 +0300

    create project

---
+++ manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import environ
+import sys
+
+if __name__ == '__main__':
+    environ.Env.read_env()
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
