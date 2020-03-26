c03e678e339f6c61a178781e96dc56aa15fb8572
slat <m89817380346>
2020-03-26 21:21:25 +0300

    production fix

--- olimp_inside/settings/production.py
+++ olimp_inside/settings/production.py
@@ -2,5 +2,9 @@ from .base import *
 
 DEBUG = True
 STATIC_ROOT = "/srv/olimp-inside/public/static/"
-
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+        os.path.join(BASE_DIR, "../ui/public/")
+]
 ALLOWED_HOSTS = ["olimp2018.enhancelab.ru", "192.168.10.4", "inside.olimp-union.com"]
+MEDIA_ROOT = "/srv/olimp-inside/public/media/"
